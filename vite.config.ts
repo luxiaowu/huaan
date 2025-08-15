795d81674926cbfdb80efe51f2d07d53ceff8592
luxiaowu <593419773@qq.com>
2025-08-16 02:24:23 +0800

    feat: 888

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'huaan',
+  base: '/huaan',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0', // 允许外部访问
